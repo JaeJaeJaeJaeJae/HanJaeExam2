bb59d91936bec6d2f800edb5cac4c0d662f47e4c
lmahimer <2015081420@ust-ics.mygbiz.com>
2018-10-04 15:34:38 +0800

    Initial commit

---
+++ app/src/main/java/com/han/jae/secondActivity.java
@@ -0,0 +1,25 @@
+package com.han.jae;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class secondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void process(View v) {
+        Intent i = null, chooser = null;
+        if (v.getId() == R.id.button2) {
+            i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+
+    }
+
+}
